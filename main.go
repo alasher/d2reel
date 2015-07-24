6c5b94fec076bce1d1ce9bee2144bf45608cd011
Austin Lasher <austinlasher@knights.ucf.edu>
2015-07-24 15:53:08 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+  
+  "github.com/alasher/d2reel/parser"
+)
+
+const VERSION = "1.0.0"
+
+func main() {
+  
+  if len(os.Args) < 2 {
+    fmt.Println("No armguments, brother! Run 'd2reel help' for argument options.")
+    os.Exit(0)
+  }
+  
+  var ids []string
+  
+  for _, v := range os.Args[1:] {
+    if strings.ToLower(v) == "help" {
+      printHelpOptions()
+      os.Exit(0)
+    }
+    
+    switch strings.ToLower(v) {
+    case "help":
+      printHelpOptions()
+      os.Exit(0)
+    case "--version":
+      printVersion()
+      os.Exit(0)
+    default:
+      ids = append(ids, v)
+    }
+  }
+  
+  parser.Parse(ids);
+  
+}
+
+func printHelpOptions() {
+  fmt.Print("Hey, friend.\nd2reel match_id0+ [flag option]*\n\n")
+  fmt.Print("You can place any number of match ids, d2reel will parse them all.\nFlags can appear anywhere in the argument list, not just after all the match ids.\n\n");
+  fmt.Print("Flag options:\n")
+  fmt.Printf("%10s - Brings up this help menu. You must have used it at least once now. Nice!\n", "help")
+  fmt.Printf("%10s - Displays the current d2reel version.\n", "--version")
+}
+
+func printVersion() {
+  fmt.Printf("You're running d2reel v%v\n", VERSION)
+}
---
+++ parser/parser.go
@@ -0,0 +1,9 @@
+package parser
+
+import (
+  "fmt"
+)
+
+func Parse(ids []string) {
+  fmt.Printf("Match ids are: %v\n", ids)
+}
